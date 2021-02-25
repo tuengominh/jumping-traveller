6be090fbf592d0e73d8da4719cd1a67914da1d4b
tuengominh <tue.ngominh@gmail.com>
2021-02-25 01:54:57 +0100

    add fixed obstacles

---
+++ Assets/Scripts/CollisionDetection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionDetection : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Building")
+        {
+            // Restart
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
---
+++ Assets/Scripts/ObstacleController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour
+{
+    public float speed = 0;
+    public float switchTime = 2;
+
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+        InvokeRepeating("Switch", 0, switchTime);
+    }
+
+    void Switch() {
+        GetComponent<Rigidbody2D>().velocity *= -1;
+    }
+}
